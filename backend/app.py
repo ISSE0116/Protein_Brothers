d20b076c0a8aee867f28ea18f22408e04ea951d4
ISSE0116 <yukizaru.2587@icloud.com>
2024-09-17 16:54:01 +0900

    edit frondend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
-import sys
-
-from flask import Flask,request
+from flask import Flask, jsonify
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def index():
-    return "Hello, World"
 
-if __name__=="__main__":
-    app.run()
+# サンプルデータ
+users = [
+    {
+        "id": 1,
+        "username": "user1",
+        "account_number": "1234567890",
+        #"icon_url": "https://example.com/icons/user1.png"
+    }
+]
 
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
