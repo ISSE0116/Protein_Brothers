e9b9c62a0d62ec6b9ad853c53158f435dd6d0c30
ISSE0116 <yukizaru.2587@icloud.com>
2024-09-19 13:09:50 +0900

    update
    ぷ

--- frontend/src/TransferPage.js
+++ frontend/src/TransferPage.js
@@ -15,7 +15,6 @@ const TransferPage = () => {
   console.log("User Context Data:", user);
 
   useEffect(() => {
-    const loggedInUserId = 1; // 実際にはログインしているユーザーIDを使用
     fetch('http://localhost:5000/api/recipients', {
       method: 'POST',
       headers: {
@@ -37,7 +36,7 @@ const TransferPage = () => {
         setError(error);
         setLoading(false);
       });
-  }, []);
+  }, [user.id]);
 
   const handleRecipientClick = (id) => {
     navigate(`/send/${id}`); // クリックされたユーザーのIDを送金ページに渡す
