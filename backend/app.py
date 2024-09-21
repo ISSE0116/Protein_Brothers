5b8b8783c76da04352c79d3691c7b6ba09af43a1
ISSE0116 <yukizaru.2587@icloud.com>
2024-09-21 16:11:25 +0900

    update
    ぷ

--- backend/app.py
+++ backend/app.py
@@ -29,11 +29,8 @@ def test():
 def login():
     # リクエストボディからIDとパスワードを取得
     data = request.json
-    #print(data)
     user_id = data.get('id')
     password = data.get('password')
-    #print(type(user_id))
-    #print(type(password))
 
     user = []
     try:
@@ -42,25 +39,19 @@ def login():
         # 引数として実行クエリを入力
         query = "SELECT * FROM users WHERE id = %s AND password = %s;"
 
-        #print(query)
         cursor.execute(query, (user_id, password))
         # クエリの実行によって得たデータをリスト形式で取得
         user = cursor.fetchone()
         print(user)
 
         close_SQL.final(connection, cursor)
-        #print(user)
-        #user[3] = base64.b64decode(user[3])
         if user:
             
             print('case1')
         # タプルを辞書型に変換
-            #user_dict = dict(zip(('id', 'username', 'account_number', 'icon', 'balance', 'password'), user))
             encoded_icon = base64.b64encode(user[3]).decode('utf-8') # user_dictを作成
             user_dict = dict(zip(('id', 'username', 'account_number', 'icon', 'balance', 'password'), [user[0], user[1], user[2], encoded_icon, user[4], user[5]]))
-            #print('case1')
             user_dict['result'] = True
-            #print('case1')
             print(user_dict)
             return jsonify(user_dict), 200
         else:
@@ -175,7 +166,7 @@ def remittance():
 
 
 
-@app.route('/api/post_billing', method=['POST'])
+@app.route('/api/post_billing', methods=['POST'])
 def post_billing():
     data = request.json
     bill = data.get('bill')
